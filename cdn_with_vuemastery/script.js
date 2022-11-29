dc234d89e0733e7c66c57bbab0ba74f7fd7ac8cc
E-K8 <ekatplat@gmail.com>
2022-11-29 15:55:55 +0000

    start another cdn practice, separate scrimba and vuemastery projects into different folders

---
+++ cdn_with_vuemastery/script.js
@@ -0,0 +1,8 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    image: './assets/vmSocks-green-onWhite.jpg',
+    link: 'https://www.amazon.co.uk/s?k=socks&crid=1O6JFBN6VDNLK&sprefix=socks%2Caps%2C73&ref=nb_sb_noss_1',
+  },
+});
