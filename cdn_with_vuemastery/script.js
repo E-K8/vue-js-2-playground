0b9b986c64eb25e0801f9b7f4959062d53e7fee8
E-K8 <ekatplat@gmail.com>
2022-11-30 11:22:24 +0000

    split product into 2 components: product and details

--- cdn_with_vuemastery/script.js
+++ cdn_with_vuemastery/script.js
@@ -1,3 +1,19 @@
+//Create a new component for product-details with a prop of details.
+
+Vue.component('product-details', {
+  props: {
+    details: {
+      type: Array,
+      required: true,
+    },
+  },
+  template: `
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+  `,
+});
+
 Vue.component('product', {
   props: {
     premium: {
@@ -6,89 +22,74 @@ Vue.component('product', {
     },
   },
   template: `
-  <div class="product">
-  <div class="product-image">
-    <img :src="image" alt="" />
-  </div>
-  <div class="product-info">
-    <h1>{{title}}</h1>
-    <!-- <p v-if="inventory > 10">In Stock</p> -->
-    <!-- <p v-else-if="inventory <= 10 && inventory > 0">Almost Sold Out!</p> -->
-    <!-- <p v-show="inStock">In Stock</p> -->
-    <!-- ^^^ toggles visibility off and on -->
-    <p v-if="inStock">In Stock</p>
-    <p v-else :class="{outOfStock:!inStock}">Out of Stock</p>
-    <p>Shipping: {{shipping}}</p>
-    <p>{{sale}}</p>
-    <!-- <span v-if="onSale">On Sale!</span> -->
-    <a :href="link" target="_blank">More products like this</a>
-    <ul>
-      <li v-for="detail in details">{{detail}}</li>
-    </ul>
-    <ul>
-      <li v-for="size in sizes">{{ size }}</li>
-    </ul>
-    <div
-      v-for="(variant, index) in variants"
-      :key="variant.variantId"
-      class="color-box"
-      :style="{backgroundColor: variant.variantColour }"
-      @mouseover="updateProduct(index)"
-    ></div>
-  </div>
+   <div class="product">
+        
+      <div class="product-image">
+        <img :src="image" />
+      </div>
 
-  <div class="cart">
-    <p>Cart ({{cart}})</p>
-  </div>
-  <button
-    v-on:click="addToCart"
-    :disabled="!inStock"
-    :class="{disabledButton: !inStock}"
-  >
-    Add to cart
-  </button>
-  <button @click="removeFromCart">Remove from cart</button>
-</div>`,
+      <div class="product-info">
+          <h1>{{ product }}</h1>
+          <p v-if="inStock">In Stock</p>
+          <p v-else>Out of Stock</p>
+          <p>Shipping: {{ shipping }}</p>
+
+          <product-details :details="details"></product-details>
+
+          <div class="color-box"
+               v-for="(variant, index) in variants" 
+               :key="variant.variantId"
+               :style="{ backgroundColor: variant.variantColor }"
+               @mouseover="updateProduct(index)"
+               >
+          </div> 
+
+          <button v-on:click="addToCart" 
+            :disabled="!inStock"
+            :class="{ disabledButton: !inStock }"
+            >
+          Add to cart
+          </button>
+
+          <div class="cart">
+            <p>Cart({{ cart }})</p>
+          </div>
+
+       </div>  
+    
+    </div>
+   `,
   data() {
     return {
-      brand: 'Socktopus',
       product: 'Socks',
+      brand: 'Vue Mastery',
       selectedVariant: 0,
-      link: 'https://www.amazon.co.uk/s?k=socks&crid=1O6JFBN6VDNLK&sprefix=socks%2Caps%2C73&ref=nb_sb_noss_1',
-
-      // onSale: true,
-      // inventory: 8,
-      details: ['80% cotton', '20% polyester', 'gender-neutral'],
+      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
       variants: [
         {
           variantId: 2234,
-          variantColour: '#228E54',
-          variantImage: './assets/vmSocks-green-onWhite.jpg',
+          variantColor: '#228E54',
+          variantImage:
+            'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
           variantQuantity: 10,
         },
         {
           variantId: 2235,
-          variantColour: '#3A5068',
-          variantImage: './assets/vmSocks-blue-onWhite.jpg',
+          variantColor: '#3A5068',
+          variantImage:
+            'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
           variantQuantity: 0,
         },
       ],
-      sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
       cart: 0,
-      onSale: true,
     };
   },
   methods: {
-    addToCart() {
+    addToCart: function () {
       this.cart += 1;
     },
-
-    updateProduct(index) {
+    updateProduct: function (index) {
       this.selectedVariant = index;
-      console.log(index);
-    },
-    removeFromCart() {
-      this.cart -= 1;
     },
   },
   computed: {
@@ -107,16 +108,10 @@ Vue.component('product', {
       }
       return 2.99;
     },
-    sale() {
-      if (this.onSale) {
-        return this.brand + ' ' + this.product + ' are on sale!';
-      }
-      return this.brand + ' ' + this.product + ' are not on sale!';
-    },
   },
 });
 
-let app = new Vue({
+var app = new Vue({
   el: '#app',
   data: {
     premium: true,
