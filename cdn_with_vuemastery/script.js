51e3dfdc5f504fc3cddfbfba79dc958d215b8c53
E-K8 <ekatplat@gmail.com>
2022-11-30 10:41:19 +0000

    computed properties for the socks app

--- cdn_with_vuemastery/script.js
+++ cdn_with_vuemastery/script.js
@@ -1,38 +1,60 @@
 let app = new Vue({
   el: '#app',
   data: {
+    brand: 'Socktopus',
     product: 'Socks',
-    image: './assets/vmSocks-green-onWhite.jpg',
+    selectedVariant: 0,
     link: 'https://www.amazon.co.uk/s?k=socks&crid=1O6JFBN6VDNLK&sprefix=socks%2Caps%2C73&ref=nb_sb_noss_1',
-    inStock: true,
+
     // onSale: true,
     // inventory: 8,
     details: ['80% cotton', '20% polyester', 'gender-neutral'],
     variants: [
       {
         variantId: 2234,
-        variantColour: 'green',
+        variantColour: '#228E54',
         variantImage: './assets/vmSocks-green-onWhite.jpg',
+        variantQuantity: 10,
       },
       {
         variantId: 2235,
-        variantColour: 'blue',
+        variantColour: '#3A5068',
         variantImage: './assets/vmSocks-blue-onWhite.jpg',
+        variantQuantity: 0,
       },
     ],
     sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
     cart: 0,
+    onSale: true,
   },
   methods: {
     addToCart() {
       this.cart += 1;
     },
 
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      this.selectedVariant = index;
+      console.log(index);
     },
     removeFromCart() {
       this.cart -= 1;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    sale() {
+      if (this.onSale) {
+        return this.brand + ' ' + this.product + ' are on sale!';
+      }
+      return this.brand + ' ' + this.product + ' are not on sale!';
     },
   },
 });
